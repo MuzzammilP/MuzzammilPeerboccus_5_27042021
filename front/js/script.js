d912e132808dbc509b8706fa49dc59871d78bec2
Muzzammil Peerboccus <muzzammil.peerboccus@gmail.com>
2022-01-28 20:24:30 +0400

    ajout canapé

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,31 @@
 let itemsData = [];
 
 const fetchItems = async () => {
-	await fetch("http://localhost:3000/api/products").then((res) => console.log(res.json()));
+	await fetch("http://localhost:3000/api/products")
+		.then((res) => res.json())
+
+		.then((promiseItems) => {
+			itemsData = promiseItems;
+			console.log(itemsData);
+		});
 };
 
-fetchItems();
+const itemsDisplay = async () => {
+	await fetchItems();
+
+	document.getElementById("items").innerHTML = itemsData.map(
+		(canape) => `
+	<div id="items${canape._id}" class="items">
+	<article>
+		<img src="${canape.imageUrl}"></img>
+		<h3>${canape.name}</h3>
+		<p>${canape.description}</p>
+	</article>
+	
+	
+	</div>
+	`
+	);
+};
+
+itemsDisplay();
