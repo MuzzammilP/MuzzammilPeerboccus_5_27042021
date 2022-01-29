33a3386e11700c2e96630c5476231b70400c0374
Muzzammil Peerboccus <muzzammil.peerboccus@gmail.com>
2022-01-29 20:21:39 +0400

    ajout page produit

---
+++ front/js/product.js
@@ -0,0 +1,30 @@
+let params = new URL(window.location.href).searchParams;
+let newID = params.get("id");
+
+const image = document.getElementsByClassName("item__img");
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const colors = document.getElementById("colors");
+
+let imageURL = "";
+let imageAlt = "";
+
+fetch("http://localhost:3000/api/products/" + newID)
+	.then((res) => res.json())
+	.then((data) => {
+		image[0].innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+		imageURL = data.imageUrl;
+		imageAlt = data.altTxt;
+		title.innerHTML = `<h1>${data.name}</h1>`;
+		price.innerText = `${data.price}`;
+		description.innerText = `${data.description}`;
+
+		// je configure le choix des couleurs
+		for (number in data.colors) {
+			colors.options[colors.options.length] = new Option(
+				data.colors[number],
+				data.colors[number]
+			);
+		}
+	});
